737c2ecf0c25cc8d1ca5a8500d556017f9508840
dudley delgado <arturobonilla0412@gmail.com>
2022-12-12 13:23:24 +0100

    test mouse function

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const mobileMenu = document.querySelector('.mobileMenu');
 const btnSwitch = document.querySelector('#switch')
 const btnDesktopSwitch = document.querySelector('#switch__desktop')
 let docTitle = document.title;
+let myDiv = document.getElementById("mouse");
 
 window.addEventListener('blur', ()=>{
     document.title = 'Come back :(';
@@ -27,3 +28,33 @@ btnDesktopSwitch.addEventListener('click',() => {
 function toggleMobileMenu(){
     mobileMenu.classList.toggle('inactive');
 }
+
+function isTouchDevice() {
+    try {
+      //We try to create TouchEvent. It would fail for desktops and throw error
+      document.createEvent("TouchEvent");
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+  
+  const move = (e) => {
+    //Try, catch to avoid any errors for touch screens (Error thrown when user doesn't move his finger)
+    try {
+      //PageX and PageY return the position of client's cursor from top left of screen
+      var x = !isTouchDevice() ? e.pageX : e.touches[0].pageX;
+      var y = !isTouchDevice() ? e.pageY : e.touches[0].pageY;
+    } catch (e) {}
+    //set left and top of div based on mouse position
+    myDiv.style.left = x - 20 + "px";
+    myDiv.style.top = y - 20 + "px";
+  };
+  //For mouse
+  document.addEventListener("mousemove", (e) => {
+    move(e);
+  });
+  //For touch
+  document.addEventListener("touchmove", (e) => {
+    move(e);
+  });
